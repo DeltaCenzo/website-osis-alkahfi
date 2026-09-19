26bc54d7fa8092a91721be257eeffca3fdc085e0
DeltaCenzo <legendary67x@gmail.com>
2026-09-19 15:48:41 +0700

    Delete aspirasi-config.js

--- aspirasi-config.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  "use strict";
-
-  // Konfigurasi backend Aspirasi OSIS.
-  // Jika deployment Apps Script berubah, cukup ganti URL /exec di file ini.
-  window.OSIS_ASPIRASI_CONFIG = Object.freeze({
-    appsScriptUrl: "https://script.google.com/macros/s/AKfycbzQM8KdNCWvhWoyohJa5zfUDT42uxayG-6XVK7f1vU9UA0hRiDF50LItX8927kIvAlXew/exec"
-  });
-})();
